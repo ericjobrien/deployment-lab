ffae4476cec06a3e8d79fa03cc747cdc4cb57136
Eric O'Brien <obrienjeric@gmail.com>
2021-10-07 16:15:18 -0400

    update to js file for css

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.get('/css', (req, res) => {
     res.sendFile(path.join(__dirname, '../stylesheet.css'));
 })
 
-app.use('/css', express.static(path.join(__dirname, 'public/main.js')));
+app.use('/css', express.static(path.join(__dirname, '../stylesheet.css')));
 
 const port = process.env.PORT || 4004;
 
