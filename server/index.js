787934a4d4c452751565cd0278c16a8f81c706a8
Eric O'Brien <obrienjeric@gmail.com>
2021-10-07 16:59:05 -0400

    change to index.js

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get('/css', (req, res) => {
+app.get('/stylesheet.css', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/stylesheet.css'));
 })
 
-app.use('/css', express.static(path.join(__dirname, '../public/stylesheet.css')));
+// app.use('/css', express.static(path.join(__dirname, '../public/stylesheet.css')));
 
 const port = process.env.PORT || 4004;
 
