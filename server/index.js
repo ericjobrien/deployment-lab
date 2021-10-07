1228987192152518b856e2cdd0d7d23b0948ac35
Eric O'Brien <obrienjeric@gmail.com>
2021-10-07 16:54:33 -0400

    trying to solve

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-    app.get('/css', (req, res) => {
-    res.sendFile(path.join(__dirname, '../stylesheet.css'));
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/stylesheet.css'));
 })
 
-app.use('/css', express.static(path.join(__dirname, '../stylesheet.css')));
+app.use('/css', express.static(path.join(__dirname, '../public/stylesheet.css')));
 
 const port = process.env.PORT || 4004;
 
